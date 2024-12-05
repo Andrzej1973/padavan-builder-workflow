42674b06a49bf27f5eacad419dbe9aace3e846d1
Andrzej1973 <raspberrytvpi@gmail.com>
2024-12-05 16:28:20 +0100

    Update pre-build.sh
    Отключение log wireguard debug

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,2 @@
 # Отключение log wireguard debug
-cp build_firmware.sh padavan-ng/trunk
+sed -i 's/func_enable_kernel_param "CONFIG_WIREGUARD_DEBUG"/func_disable_kernel_param "CONFIG_WIREGUARD_DEBUG"/' padavan-ng/trunk/build_firmware.sh
